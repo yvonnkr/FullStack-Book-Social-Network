e86dbe0669dbcf185c3345145af68aef5ecc1119
yvonnkr <yvonnkr86@gmail.com>
2024-09-05 17:23:07 +0100

    implement book-list component and interceptor for auth token

--- book-network-ui/src/app/app.routes.ts
+++ book-network-ui/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import {Routes} from '@angular/router';
 import {LoginComponent} from "./pages/login/login.component";
 import {RegisterComponent} from "./pages/register/register.component";
 import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
+import {MainComponent} from "./modules/book/pages/main/main.component";
 
 export const routes: Routes = [
+  {path: '', component: MainComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'activate-account', component: ActivateAccountComponent},  {
